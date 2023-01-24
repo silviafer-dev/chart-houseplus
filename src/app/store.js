7155dc0cad63e68cb0da3dafe4aca86360934d0d
Silvia Ferrai <silviafer@hotmail.it>
2023-01-24 11:16:57 +0100

    Create reducer with toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import mockDataReducer from "../features/chart/chartSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    data: mockDataReducer,
+  },
 });
